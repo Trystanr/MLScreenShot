eeab483668d091615c20f68cad726e8c6dbea60b
Trystan <trystan.b@hotmail.com>
2020-10-06 17:17:28 +0200

    Initial Commit

---
+++ MLScreenShot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MLScreenShot
+//
+//  Created by Trystan Rivers on 2020/10/06.
+//  Copyright © 2020 Trystan Rivers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MLScreenShot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MLScreenShot
+//
+//  Created by Trystan Rivers on 2020/10/06.
+//  Copyright © 2020 Trystan Rivers. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
